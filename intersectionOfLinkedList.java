d6effa97da8afb5792d7a6a0f8e768239b188f99
ShubhamDubey05 <shubhamdubey05092001@gmail.com>
2024-10-02 13:50:34 +0530

    Intersection Of LinkedList

---
+++ intersectionOfLinkedList.java
@@ -0,0 +1,44 @@
+public class intersectionOfLinkedList {
+    public class ListNode {
+        int val;
+       ListNode next;
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode tempA= headA;
+        ListNode tempB= headB;
+        int lengthA= 0;
+        while(tempA!=null){
+            lengthA++;
+            tempA=tempA.next;
+        }
+        int lengthB=0;
+         while(tempB!=null){
+            lengthB++;
+            tempB=tempB.next;
+        }
+            tempA=headA;
+            tempB=headB;
+        if(lengthA>lengthB){
+            int steps= lengthA-lengthB;
+            for(int i =1;i<=steps;i++){
+                tempA=tempA.next;
+            }
+        }
+        else{
+               int steps= lengthB-lengthA;
+            for(int i =1;i<=steps;i++){
+                tempA=tempA.next;
+                tempB=tempB.next;
+            }
+        }
+        return tempA;
+    }
+    public static void main(String[] args) {
+    
+}
+} 
+
