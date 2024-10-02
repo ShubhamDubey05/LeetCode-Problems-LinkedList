e6e908f4f5d4267337844fe70ffdb5e981340805
ShubhamDubey05 <shubhamdubey05092001@gmail.com>
2024-10-02 11:59:17 +0530

    Fisrt Problem

--- LinkedList1.java
+++ LinkedList1.java
@@ -1,6 +1,30 @@
 
+public class LinkedList1 {
 
-public class LinkedList1 {public static void main(String[] args) {
+    public class Node {
+        int val;
+        Node next;
+        public Node(int val) {
+            this.val = val;
+        }
+        
+    }
+    public static Node  nodeDeleteFromEnd(Node head,int m  ){
+                Node slow=head;
+                Node fast=head;
+                for(int i =0; i<m; i++){
+                    fast=fast.next;
+                }
+                while(fast.next!=null){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                slow.next=slow.next.next;
+
+        return slow;
+    }
+    public static void main(String[] args) {
+
     
 }
     
