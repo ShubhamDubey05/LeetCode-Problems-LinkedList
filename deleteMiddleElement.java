ec006277e7e8797ef9827ba40078d02f9faded02
ShubhamDubey05 <shubhamdubey05092001@gmail.com>
2024-10-03 12:47:24 +0530

    Delete  Middle Element From LinkedList

---
+++ deleteMiddleElement.java
@@ -0,0 +1,33 @@
+
+
+class Solution {
+     static ListNode deleteMiddle(ListNode head) {
+        if(head.next == null){
+            return null;
+        }
+        ListNode slow=head;;
+        ListNode fast =head;
+        while(fast.next.next!=null && fast.next.next.next!= null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        slow.next=slow.next.next;
+        return head;
+    }
+}
+          class ListNode{
+            int val;
+            ListNode next;
+            public ListNode(int val) {
+                this.val = val;
+            }
+            
+        }
+
+
+public class deleteMiddleElement {
+    public static void main(String[] args) {
+        
+    }
+    
+}
