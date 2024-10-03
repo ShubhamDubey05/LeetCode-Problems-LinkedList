fc9f769f26a124ecde17110fae193310c5fdf669
ShubhamDubey05 <shubhamdubey05092001@gmail.com>
2024-10-03 18:02:44 +0530

    Recursive reverse Linked List

---
+++ recursiveReverseLinkedList.java
@@ -0,0 +1,35 @@
+
+  class ListNode {
+        int val;
+         ListNode next;
+        ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+    
+   class Solution {
+   
+       public ListNode reverseList(ListNode head) {
+   
+           if(head == null){
+               return head;
+           }
+           if(head.next == null){
+               return head;
+           }
+           ListNode newnode = reverseList(head.next);
+           head.next.next = head;
+           head.next =null;
+           return newnode;
+       }
+   }
+
+
+public class recursiveReverseLinkedList {
+    
+    public static void main(String[] args) {
+    
+
+}
+    
+}
